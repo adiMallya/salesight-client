a70a42e5af2f1ab4eae337eb62083603a0b65aab
adiMallya <adimallya99@gmail.com>
2023-10-14 12:07:56 +0530

    add: Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import {
+  InventoryOutlined,
+  SellOutlined,
+  AssessmentOutlined,
+} from "@mui/icons-material";
+
+export const Navbar = () => {
+  return (
+    <nav className="w-full static top-0 p-4 flex items-center justify-center">
+      <ul className="w-full flex justify-around">
+        <li>
+          <Link to="/inventory" className="flex flex-col items-center">
+            <InventoryOutlined />
+            <span aria-label="Inventory">Inventory</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="flex flex-col items-center">
+            <AssessmentOutlined />
+            <span aria-label="Reports">Reports</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/sales" className="flex flex-col items-center">
+            <SellOutlined />
+            <span aria-label="Sales">Sales</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
