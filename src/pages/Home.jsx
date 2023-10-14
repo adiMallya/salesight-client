89666bec11b06492f086bec2e429892432c77565
adiMallya <adimallya99@gmail.com>
2023-10-14 17:39:52 +0530

    feat: display report on load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Navbar } from "src/components";
+import { ReportTabs } from "src/features/reports";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col items-center min-h-screen">
+      <main className="flex flex-col items-center gap-8 min-h-screen">
         <section className="flex flex-wrap mt-8">
           <h1 className="font-bold text-4xl">Welcome to SaleSight</h1>
         </section>
+        <ReportTabs />
       </main>
     </>
   );
