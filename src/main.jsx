21ec13c03285bae80a12e1c63664c9a654c363df
adiMallya <adimallya99@gmail.com>
2023-10-14 17:36:32 +0530

    add: added chart dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,38 @@ import { Provider } from "react-redux";
 import store from "src/redux/store";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  Chart,
+  BarController,
+  LineController,
+  BarElement,
+  PointElement,
+  LineElement,
+  PieController,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+Chart.register(
+  PieController,
+  BarController,
+  LineController,
+  ArcElement,
+  BarElement,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
