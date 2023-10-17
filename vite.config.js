b9555c42f593c04c2c19e73e4fc3329cac2276a2
adiMallya <adimallya99@gmail.com>
2023-10-17 16:36:53 +0530

    refactor: main entry point

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
@@ -23,7 +24,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
     }
   }
 })
